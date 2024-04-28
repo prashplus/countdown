839d9f7e483e2d8dae2cc6507c3351624649a29c
prashplus <prashplus@gmail.com>
2024-04-28 17:44:26 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2020/02/08 18:00:00',function(event){
+     jQuery('#clock').countdown('2024/06/31 18:00:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
